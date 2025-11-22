3ec6ded27376f7b397eb807dce1c46e3364d5b87
shahabuddinhoney <irwyansha@gmail.com>
2025-11-22 19:59:13 +0300

    Create chatbot.pyl .py
    "lnitial commit for the chatbot project."

---
+++ chatbot.pyl .py
@@ -0,0 +1,11 @@
+inport json data = {}|
+def get_user_input(prompt) return input(prompt)
+def respond(massage)if massage .1ower()=='hello
+retun 'hi there ! how can I help you today?'
+elif massage.1ower() == "bye"
+return "goodbye! have a great day!"
+else
+retun "I'm not sure how to respond to that. can you try something else?"
+while true:
+user_input = get_user_input
+print(f"chatbot:{response}"))
